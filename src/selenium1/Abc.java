90a37ff85322992c7f81f8591706eea0da055772
Elangovan <aelangovan@argogroupus.com>
2020-01-11 17:04:46 -0600

    My first git commit

---
+++ src/selenium1/Abc.java
@@ -0,0 +1,10 @@
+package selenium1;
+
+public class Abc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hgh");
+	}
+
+}
---
+++ src/selenium1/Sample.java
@@ -0,0 +1,16 @@
+package selenium1;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int a=2;
+	int b=3;
+	int c=a+b;
+	System.out.println("sum:"+c);
+	
+  
+
+	}
+
+}
